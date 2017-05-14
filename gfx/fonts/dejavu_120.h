8ca5a1763a63727320a640d3177e923e7ada4a7e
Kiss-Vamosi Gabor <kisvegabor@gamil.com>
2017-05-14 18:51:49 +0200

    font: dejavu_120 added

---
+++ gfx/fonts/dejavu_120.h
@@ -0,0 +1,18 @@
+#ifndef DEJAVU_120_H
+#define DEJAVU_120_H
+
+/*Use ISO8859-1 encoding in the IDE*/
+
+#include "misc_conf.h"
+#if  USE_FONT_DEJAVU_120 != 0
+
+
+#include <stdint.h>
+#include "../font.h"
+
+
+const font_t * dejavu_120_get_dsc(void);
+
+#endif
+
+#endif
