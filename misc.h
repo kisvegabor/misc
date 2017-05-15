41cb7dcbd60b8b4373b01405af9a27f601cc6a27
Gabor <kisvegabor@gmail.com>
2017-05-15 09:49:57 +0200

    release v4.0.0

--- misc.h
+++ misc.h
@@ -13,7 +13,7 @@
 /*********************
  *      DEFINES
  *********************/
-#define MISC_VERSION_MAJOR  3
+#define MISC_VERSION_MAJOR  4
 #define MISC_VERSION_MINOR  0
 #define MISC_VERSION_PATCH  0
 
