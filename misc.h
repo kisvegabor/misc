715cffa97d4eb71e174093be9141db82ff2216fb
Kiss-Vamosi Gabor <kisvegabor@gamil.com>
2017-07-09 15:34:08 +0200

    release v4.1.0

--- misc.h
+++ misc.h
@@ -18,7 +18,7 @@ extern "C" {
  *      DEFINES
  *********************/
 #define MISC_VERSION_MAJOR  4
-#define MISC_VERSION_MINOR  0
+#define MISC_VERSION_MINOR  1
 #define MISC_VERSION_PATCH  1
 
 /**********************
